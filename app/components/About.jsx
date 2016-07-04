7e9634a397b7ede1e5e37dab7e29fe336b13cef4
Michael Loschiavo <losch103@gmail.com>
2016-07-03 22:31:46 -0700

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,28 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+        <h1 className="text-center">About</h1>
+        <p>This is a basic React application which fetches weather data from a weather API. Below are the resources used for this application:</p>
+      <ol>
+        <li>
+          <a target="_blank" href='https://facebook.github.io/react/index.html'>React</a>
+        </li>
+        <li>
+          <a target="_blank" href='http://openweathermap.org/'>OpenWeatherMap</a>
+        </li>
+      </ol>
+      <p>This application is publicly visible on the following sites:</p>
+      <ul>
+        <li>
+          <a target="_blank" href="https://github.com/loschtreality/ReactWeather">Github</a>
+        </li>
+        <li>
+          <a target="_blank" href="http://morning-sea-29825.herokuapp.com/#/?_k=cpmozr">Heroku</a>
+        </li>
+      </ul>
     </div>
   )
 }
